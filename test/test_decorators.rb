18d14b2db1dd0d82d810366f45ee032ca0228f93
Fabio Kung <fabio.kung@gmail.com>
2012-04-26 19:28:20 -0700

    more tests

--- test/test_decorators.rb
+++ test/test_decorators.rb
@@ -8,22 +8,30 @@ class MyService
 
   log_context "myservice", release: "v51"
 
+  def class_log_context
+    log
+  end
+
   def only_tags
     log "only", "tags"
   end
 
+  def only_hash
+    log from: "hash", size: 2
+  end
+
   def basic_logging
-    log "basic", "that", "works", data: "useful"
+    log "basic", "that", "works", data: "useful", number: 10
   end
 
   log
   def basic_wrap_with_logging
-    sleep 1
+    sleep 0.1
   end
 
   log "wrap_with_more_context", rule: "always"
   def wrap_with_more_context
-    sleep 1
+    sleep 0.1
   end
 
 end
@@ -31,15 +39,31 @@ end
 
 class TestDecorators < MiniTest::Unit::TestCase
 
+  @@output, writer = IO.pipe
+  Scrolls::Log.start(writer)
+
   def setup
-    @output, writer = IO.pipe
     @service = MyService.new
-    Scrolls::Log.start(writer)
+  end
+
+  def test_class_log_context
+    @service.class_log_context
+    assert_equal "myservice release=v51\n", @@output.gets
   end
 
   def test_converts_tags
     @service.only_tags
-    assert_equal "myservice release=v51 only tags\n", @output.gets
+    assert_equal "myservice release=v51 only tags\n", @@output.gets
+  end
+
+  def test_accepts_only_hash
+    @service.only_hash
+    assert_equal "myservice release=v51 from=hash size=2\n", @@output.gets
+  end
+
+  def test_accepts_tags_and_hash
+    @service.basic_logging
+    assert_equal "myservice release=v51 basic that works data=useful number=10\n", @@output.gets
   end
 
 end
