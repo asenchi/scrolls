143b8c5bf496638f8451623c2291e34be8db40d7
Fabio Kung <fabio.kung@gmail.com>
2012-04-26 19:09:35 -0700

    decorators working implementation

--- test/test_decorators.rb
+++ test/test_decorators.rb
@@ -1,33 +1,46 @@
-require "stringio"
 require "minitest/autorun"
 
 $:.unshift File.expand_path(File.join("..", "lib"))
 require "scrolls/decorators"
 
-class TestDecorators < MiniTest::Unit::TestCase
+class MyService
+  include Scrolls::Decorators
 
-  class MyService
-    include Scrolls::Decorators
+  log_context "myservice", release: "v51"
 
-    log_context "myservice", release: "v51"
-
-    def basic_logging
-      log "basic", data: "useful"
-    end
-
-    log
-    def basic_wrap_with_logging
-      sleep 1
-    end
-
-    log "wrap_with_more_context", rule: "always"
-    def wrap_with_more_context
-      sleep 1
-    end
-
+  def only_tags
+    log "only", "tags"
   end
 
+  def basic_logging
+    log "basic", "that", "works", data: "useful"
+  end
 
+  log
+  def basic_wrap_with_logging
+    sleep 1
+  end
+
+  log "wrap_with_more_context", rule: "always"
+  def wrap_with_more_context
+    sleep 1
+  end
+
+end
+
+
+class TestDecorators < MiniTest::Unit::TestCase
+
+  def setup
+    @output, writer = IO.pipe
+    @service = MyService.new
+    Scrolls::Log.start(writer)
+  end
+
+  def test_converts_tags
+    @service.only_tags
+    assert_equal "myservice release=v51 only tags\n", @output.gets
+  end
 
 end
 
