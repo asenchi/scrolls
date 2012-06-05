9243e6400e8078af0a916a4d3a2ce0790a1be3f2
Curt Micol <asenchi@asenchi.com>
2012-06-05 16:24:34 -0400

    Move levels into Log

--- lib/scrolls/log.rb
+++ lib/scrolls/log.rb
@@ -1,4 +1,3 @@
-require "scrolls/levels"
 require "scrolls/parser"
 require "scrolls/utils"
 
@@ -6,9 +5,20 @@ module Scrolls
   module Log
     extend self
 
-    extend Levels
     extend Parser
     extend Utils
+
+    LOG_LEVEL = (ENV['LOG_LEVEL'] || 3).to_i
+    LOG_LEVEL_MAP = {
+      "emergency" => 0,
+      "alert"     => 1,
+      "critical"  => 2,
+      "error"     => 3,
+      "warning"   => 4,
+      "notice"    => 5,
+      "info"      => 6,
+      "debug"     => 7
+    }
 
     def stream=(out=nil)
       @defined = out.nil? ? false : true
@@ -119,5 +129,14 @@ module Scrolls
         end
       end
     end
+
+    def log_level_ok?(level)
+      if level
+        LOG_LEVEL_MAP[level.to_s] <= LOG_LEVEL
+      else
+        true
+      end
+    end
+
   end
 end
