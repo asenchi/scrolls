5be1cd2d83f8cebfa3de138ca5e351f02b975811
Curt Micol <asenchi+github@asenchi.com>
2014-02-06 11:55:47 -0500

    Make our parser tests work on 1.8.7

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -1,81 +1,81 @@
-require_relative "test_helper"
+require File.expand_path("../test_helper", __FILE__)
 
 class TestScrollsParser < Test::Unit::TestCase
   include Scrolls::Parser
 
   def test_parse_bool
-    data = { test: true, exec: false }
+    data = { :test => true, :exec => false }
     assert_equal "test=true exec=false", unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
   end
 
   def test_parse_numbers
-    data = { elapsed: 12.00000, __time: 0 }
+    data = { :elapsed => 12.00000, :__time => 0 }
     assert_equal "elapsed=12.000 __time=0", unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
   end
 
   def test_parse_strings
     # Strings are all double quoted, with " or \ escaped
-    data = { s: "echo 'hello' \"world\"" }
+    data = { :s => "echo 'hello' \"world\"" }
     assert_equal 's="echo \'hello\' \\"world\\""', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = { s: 'echo "hello"' }
+    data = { :s => 'echo "hello"' }
     assert_equal "s='echo \"hello\"'", unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = { s: "hello world" }
+    data = { :s => "hello world" }
     assert_equal 's="hello world"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = { s: "hello world".inspect }
+    data = { :s => "hello world".inspect }
     assert_equal "s='\"hello world\"'", unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = { s: "hello=world".inspect }
+    data = { :s => "hello=world".inspect }
     assert_equal "s='\"hello=world\"'", unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = { s: "slasher \\" }
+    data = { :s =>  "slasher \\" }
     assert_equal 's="slasher \\\\"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = {s: "x=4,y=10" }
+    data = { :s => "x=4,y=10" }
     assert_equal 's="x=4,y=10"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = {s: "x=4, y=10" }
+    data = { :s => "x=4, y=10" }
     assert_equal 's="x=4, y=10"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = {s: "x:y" }
+    data = { :s => "x:y" }
     assert_equal 's="x:y"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
-    data = {s: "x,y" }
+    data = { :s => "x,y" }
     assert_equal 's="x,y"', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
 
     # simple value is unquoted
-    data = { s: "hi" }
+    data = { :s => "hi" }
     assert_equal 's=hi', unparse(data)
     assert_equal data.inspect, parse(unparse(data)).inspect
   end
 
   def test_unparse_constants
-    data = { s1: :symbol, s2: Scrolls }
+    data = { :s1 => :symbol, :s2 => Scrolls }
     assert_equal "s1=symbol s2=Scrolls", unparse(data)
   end
 
   def test_unparse_nil
-    data = { n: nil }
+    data = { :n => nil }
     assert_equal "n=nil", unparse(data)
   end
 
   def test_unparse_time
     time = Time.new(2012, 06, 19, 16, 02, 35, "+01:00")
-    data = { t: time }
+    data = { :t => time }
     assert_equal 't="2012-06-19T16:02:35+01:00"', unparse(data)
   end
 
